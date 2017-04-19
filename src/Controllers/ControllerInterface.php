c4c2afaa1c7eacfb50098c7c12c27ec0fd54f517
yoeunes <younes.khoubza@gmail.com>
2017-04-19 20:05:59 +0100

    create ControllerInterface

---
+++ src/Controllers/ControllerInterface.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Yakuzan\Boiler\Controllers;
+
+use Illuminate\Http\Request;
+use Yakuzan\Boiler\Services\ServiceInterface;
+
+interface ControllerInterface
+{
+    public function index();
+
+    public function create();
+
+    /**
+     * @param Request $request
+     */
+    public function store(Request $request);
+
+    /**
+     * @param int $id
+     */
+    public function show(int $id);
+
+    /**
+     * @param int $id
+     */
+    public function edit(int $id);
+
+    /**
+     * @param Request $request
+     * @param int $id
+     */
+    public function update(Request $request, int $id);
+
+    /**
+     * @param int $id
+     */
+    public function destroy(int $id);
+
+    /**
+     * @return ServiceInterface
+     */
+    public function getService(): ServiceInterface;
+}
