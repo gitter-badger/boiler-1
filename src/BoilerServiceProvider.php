55d611b4ce78baba000803501c294d054b891fdd
yoeunes <younes.khoubza@gmail.com>
2017-04-19 20:02:53 +0100

    create BoilerServiceProvider and register services and aliases

---
+++ src/BoilerServiceProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Yakuzan\Boiler;
+
+use Illuminate\Support\ServiceProvider;
+
+class BoilerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'../vendor/yajra/laravel-datatables-oracle/src/config/datatables.php' => config_path('datatables.php'),
+            __DIR__.'../vendor/dingo/api/config/api.php' => config_path('api.php'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerServices();
+        $this->registerAliases();
+    }
+
+    private function registerServices()
+    {
+        if ('production' !== $this->app->environment()) {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
+
+        $this->app->register(\Collective\Html\HtmlServiceProvider::class);
+        $this->app->register(\Yajra\Datatables\DatatablesServiceProvider::class);
+//        $this->app->register(\Dingo\Api\Provider\LaravelServiceProvider::class);
+    }
+
+    private function registerAliases()
+    {
+        if (class_exists(\Illuminate\Foundation\AliasLoader::class)) {
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+            $loader->alias('Form', \Collective\Html\FormFacade::class);
+            $loader->alias('Html', \Collective\Html\HtmlFacade::class);
+        }
+    }
+}
