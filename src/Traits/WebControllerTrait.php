cc6d14899f791be3306752d64aee1865e9f14045
yoeunes <younes.khoubza@gmail.com>
2017-04-19 20:05:11 +0100

    web controller Trait

---
+++ src/Traits/WebControllerTrait.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Yakuzan\Boiler\Traits;
+
+use Illuminate\Http\Response;
+use function view;
+
+trait WebControllerTrait
+{
+    use ControllerTrait;
+
+    protected $dataTable = null;
+
+    /** @var Response */
+    protected $view = null;
+
+    public function getDataTable()
+    {
+        return $this->dataTable;
+    }
+
+    public function setDataTable($dataTable)
+    {
+        $this->dataTable = $dataTable;
+    }
+
+    /**
+     * @param string|null $view
+     * @param array $data
+     * @param array $mergeData
+     *
+     * @return \Illuminate\Contracts\View\Factory|Response|\Illuminate\View\View
+     */
+    public function getView(string $view = null, array $data = [], array $mergeData = [])
+    {
+        if (null !== $this->view) {
+            return $this->view;
+        }
+
+        $folder = strtolower(preg_replace('/App\\\/', '', $this->entity));
+
+        if (view()->exists($folder.'.'.$view)) {
+            return view($folder.'.'.$view, $data, $mergeData);
+        }
+
+        return null;
+    }
+
+    public function setView($view)
+    {
+        $this->view = $view;
+    }
+}
