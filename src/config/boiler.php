18d4e9ae60239d265dd114e870457cdbec371454
yoeunes <younes.khoubza@gmail.com>
2017-05-04 10:55:01 +0100

    add datatable namespace to config file

--- src/config/boiler.php
+++ src/config/boiler.php
@@ -7,6 +7,8 @@ return [
     'services_namespace'     => 'App\\Services',
     'policies_namespace'     => 'App\\Policies',
     'views_path'             => resource_path('views'),
+    'datatable_namespace'    => 'App\\DataTables',
+    'datatable_scope_namespace'    => 'App\\DataTables\\Scopes',
     'exceptions'             => [
         \Illuminate\Auth\Access\AuthorizationException::class       => ['method' => 'unauthorized', 'message' => null],
         \Illuminate\Database\Eloquent\ModelNotFoundException::class => ['method' => 'notFound', 'message' => null],
